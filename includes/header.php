f06c4ffe934f4d207261cea20ecade826fb3cd6c
Keza <kezaspr@users.noreply.github.com>
2026-02-01 17:16:37 +0700

    db upd, btns, cats, etc
    newest sql clair4uu.sql
    prod db removed

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>clair4UDIO - Toko Perangkat Audio Terbaik</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <!-- Header -->
